2992f3826838d35e59e82daa78fca0a5b10e35a9
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2017-12-21 12:30:21 -0500

    input validation

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value,
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			};
 		},
 		addListItem: function(obj, type) {
@@ -101,7 +101,9 @@ var UIController = (function() {
 
 			fieldsArr.forEach(function(current, index, array) {
 				current.value = "";
-			})
+			});
+
+			fieldsArr[0].focus();
 		},
 		getDOMstrings: function(){
 			return DOMstrings;
@@ -123,6 +125,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 				ctrlAddItem();
 			}
 		});
+	};
+
+	var updateBudget = function(){
+		// 1. Calculate the budget
+
+		// 2. Return the budget
+
+		// 3. Dispaly the budget on the UI
 	}
 
 	var ctrlAddItem = function(){
@@ -130,20 +140,21 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 1. Get the field input data
 		input = UICtrl.getInput();
 
-		// 2. Add the item to the budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to the budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		// 3. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			// 3. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 
-		// 4. Clear the fields
-		UICtrl.clearFields();
+			// 4. Clear the fields
+			UICtrl.clearFields();
 
-		// 5. Calculate the budget
-
-		// 6. Dispaly the budget on the UI
-
-	}
+			// 5. Calculate and update budget
+			updateBudget();
+		}
+		
+	};
 
 		return {
 			init: function() {
