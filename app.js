5087c4f2fddc0e1dc0760d229243e547e48ec27b
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2017-12-21 12:49:05 -0500

    calculate income and percentages

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
 		this.value = value;
 	}
 
+	var calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(current) {
+			sum += cur.value
+		});
+		data.totals[type] = sum;
+	}
+
 	var data = {
 		allItems: {
 			exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		},
+		budget: 0,
+		percentage: -1
 	}
 
 	return {
@@ -46,8 +56,27 @@ var budgetController = (function() {
 			data.allItems[type].push(newItem);
 			// Return the new element
 			return newItem;
-			
+		},
+		calculateBudget: function() {
+
+			// Calculate total income and expenses
+			calculateTotal('inc');
+			calculateTotal('exp');
+
+			// Calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			// Calculate the percentage of income that we spent
+			data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+		},
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp
+			}
 		}
+		
 	}
 
 })();
@@ -129,8 +158,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 	var updateBudget = function(){
 		// 1. Calculate the budget
+		budgetCtrl.calculateBudget();
 
 		// 2. Return the budget
+		var budget = budgetCtrl.getBudget();
 
 		// 3. Dispaly the budget on the UI
 	}
