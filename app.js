6e99e75380f6d56ff907ad28db18aca5f817d633
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2017-12-21 12:16:39 -0500

    clear input fields

--- app.js
+++ app.js
@@ -92,6 +92,17 @@ var UIController = (function() {
 			document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
 
 		},
+		clearFields: function() {
+			var fields, fieldsArr;
+
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			fieldsArr.forEach(function(current, index, array) {
+				current.value = "";
+			})
+		},
 		getDOMstrings: function(){
 			return DOMstrings;
 		}
@@ -125,9 +136,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 3. Add the item to the UI
 		UICtrl.addListItem(newItem, input.type);
 
-		// 4. Calculate the budget
+		// 4. Clear the fields
+		UICtrl.clearFields();
 
-		// 5. Dispaly the budget on the UI
+		// 5. Calculate the budget
+
+		// 6. Dispaly the budget on the UI
 
 	}
 
